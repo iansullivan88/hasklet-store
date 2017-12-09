e9e612ad422f61b77271ac9eb6bdc4917865756b
Ian Sullivan <iansullivan88@gmail.com>
2017-12-09 22:21:51 +0000

    Added cli args

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,41 @@ import           Hasklet.Store.Database
 import           Hasklet.Store.Types
 import           Hasklet.Store.Web
 
+import           Data.Monoid
 import           Network.Wai.Handler.Warp
+import           Options.Applicative
 import           System.Directory
 import           System.FilePath
 
+data Options = Options {
+    port         :: Int,
+    databasePath :: String
+}
+
 main :: IO ()
 main = do
-    let port = 8000
     curDir <- getCurrentDirectory
-    let path = curDir </> "content-store.db"
+    let defaultPath = curDir </> "content-store.db"
+        defaultPort = 80
+    opt <- execParser (info (optionParser defaultPort defaultPath) mempty)
+    let path = databasePath opt
+        p = port opt
+    putStrLn "Starting hasklet-store"
+    putStrLn $ "port: " ++ show p
+    putStrLn $ "database: " ++ show path
     da <- createDatabaseActions path 20
     withTransaction da createSchema
-    run port (application da)
+    run p (application da)
 
+optionParser :: Int -> String -> Parser Options
+optionParser dPort dPath = Options <$> portParser <*> pathParser where
+    portParser = option auto $ long "port"
+                     <> short 'p'
+                     <> metavar "PORT"
+                     <> help "port to use"
+                     <> value dPort
+    pathParser = strOption $ long "database"
+                     <> short 'd'
+                     <> metavar "DATABASE_PATH"
+                     <> help "path to the sqlite database"
+                     <> value dPath
